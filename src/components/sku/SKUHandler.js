5bb7a25807af9a8b78e45bd21fa4e32516d305de
afif0808 <afif.panai@gmail.com>
2020-06-18 10:17:54 +0700

    change api source url

--- src/components/sku/SKUHandler.js
+++ src/components/sku/SKUHandler.js
@@ -1,5 +1,5 @@
 function GetSKUs() {
-    return fetch("http://localhost:555/skus").then((resp) => resp.json())
+    return fetch("http://161.35.98.104:555/skus").then((resp) => resp.json())
 }
 function CreateSKus(postData){
     var req = {
@@ -9,11 +9,11 @@ function CreateSKus(postData){
         },
         body : JSON.stringify(postData)
     }
-    return fetch("http://localhost:555/skus",req).then(resp => resp.json())
+    return fetch("http://161.35.98.104:555/skus",req).then(resp => resp.json())
 
 }
 function DeleteSKUById(skuId) {
-    return fetch("http://localhost:555/skus/" + skuId , {method:"DELETE"}).then(resp => resp.json())
+    return fetch("http://161.35.98.104:555/skus/" + skuId , {method:"DELETE"}).then(resp => resp.json())
 }
 function UpdateSKUById(sku) {     
     var req = {
@@ -23,6 +23,6 @@ function UpdateSKUById(sku) {
         },
         body : JSON.stringify({sku : sku}),
     }
-    return fetch("http://localhost:555/skus/" + sku.id,req).then(resp => resp.json())
+    return fetch("http://161.35.98.104:555/skus/" + sku.id,req).then(resp => resp.json())
 }
 export {GetSKUs , CreateSKus , DeleteSKUById, UpdateSKUById} 
--- src/components/skuout/SKUOutHandler.js
+++ src/components/skuout/SKUOutHandler.js
@@ -12,11 +12,11 @@ function CreateSKUOuts(skuOuts,skuOutGroup) {
     }
     console.log(req)
 
-    return fetch("http://localhost:555/skuouts",req).then(resp => resp.json())
+    return fetch("http://161.35.98.104:555/skuouts",req).then(resp => resp.json())
 }
 
 function GetSKUOutsByGroupID(groupId) {
-    return fetch("http://localhost:555/skuoutgroups/" + groupId + "/skuouts").then((resp) => resp.json())
+    return fetch("http://161.35.98.104:555/skuoutgroups/" + groupId + "/skuouts").then((resp) => resp.json())
 }
 
 export {CreateSKUOuts , GetSKUOutsByGroupID}
